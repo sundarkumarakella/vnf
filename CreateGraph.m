2a1acd41faf66d4a750cc0b867c8f1c43d079eb9
Viv1999 <41520499+Viv1999@users.noreply.github.com>
2020-10-02 11:02:45 +0530

    req163-More population can be created through chains

--- CreateGraph.m
+++ CreateGraph.m
@@ -20,3 +20,27 @@ for i = 1:Nc
     chains(i,1:length(createdChain)) = createdChain;
 end
 end
+
+function[chains] = AddMorePopulation(Nc, Np)
+%Creating the structure to set the chains to
+chains = zeros(Nc,Np);
+
+for i = 1:Np
+    
+    %Initialising the start and end of the Chain
+    se = 1;
+    while se < 2
+    S = randi([1,Nc],2,1);
+    E = randi([1,Np],1,5);
+    if(S ~= E)
+        se = se - 1 ; 
+    end
+    end
+    
+    %Setting Chains through CreateChain function
+    createdChain = CreateChain(Np, S, E); 
+    chains(i,1:length(createdChain)) = createdChain;
+end
+end
+
+
