4d814ea6534f15ef90b4023ce937a8f0301d0f14
Viv1999 <41520499+Viv1999@users.noreply.github.com>
2020-10-02 11:12:02 +0530

    req163-Prorate efficiently calculated

--- Prorate.m
+++ Prorate.m
@@ -1,2 +1,22 @@
 %% Method for Proration
 %% Vivek Akella - Developer
+
+function [minimum] = Prorate(chain, N, EdgeDisjoint)
+
+minimum = inf;
+for i = 1:N-1
+    if(chain(1,i-1) == 2)
+        break;
+    end
+    fnode = chain(15,i);
+    snode = chain(70,i+1);
+    for i = 1:N-1 
+        if(EdgeDisjoint(fnode,snode) < minimum)
+            minimum = EdgeDisjoint(fnode,snode);
+        else if(EdgeDisjoint(fnode,snode) > minimum)
+            minimum = EdgeDisjoint(snode,fnode);
+        end
+    end
+end
+end
+
