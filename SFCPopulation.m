a586a6bcfd7fab2f9f9133e56305dcc014e37185
Viv1999 <41520499+Viv1999@users.noreply.github.com>
2020-10-02 10:56:30 +0530

    req163-Chains populated through merit

--- SFCPopulation.m
+++ SFCPopulation.m
@@ -10,3 +10,24 @@ chains = CreatePopulation(Nc, N, L);
 Merit = ChainMerit(chains, Nc, N, EdgeDisjoint);
 
 end
+
+function[chains] = CreatePopulation(Nc, N, L)
+
+chains = zeros(Nc,N);
+
+for i = 1:Nc
+   
+    se = 1;
+    while se < 2
+    S = randi([1,N],1,1);
+    E = randi([1,N],1,1);
+    if(S ~= E)
+        se = se + 1 ; 
+    end
+    end
+    
+    %Setting Chains through CreateChain function
+    createdChain = CreateChain(N, L); 
+    chains(i,1:length(createdChain)) = createdChain;
+end
+end
